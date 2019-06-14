23d683589a53c06e38ac494056035814f915899c
veyselbugraaydogan <veyselbugraaydogan@gmail.com>
2019-06-15 01:47:03 +0300

    Tagler oluşturuldu.

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-    PermissionsMixin
+                                       PermissionsMixin
+from django.conf import settings
 
 
 class UserManager(BaseUserManager):
@@ -38,3 +39,17 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Tag(models.Model):
+
+    """Tag to be used for a recipe"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+        #Buradaki cascade user silindiğinde oluşturduğu tagı da siliyor?
+    )
+
+    def __str__(self):
+        return self.name
