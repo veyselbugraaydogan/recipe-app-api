0c79744a10e41c1bfeb48568cc3ae79d3cbd7b2b
veyselbugraaydogan <veyselbugraaydogan@gmail.com>
2019-06-20 03:24:13 +0300

    Tarif ekleme özelliği eklendi.

--- app/core/models.py
+++ app/core/models.py
@@ -65,3 +65,22 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    """Recipe object"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    ingredients = models.ManyToManyField('Ingredient')
+    tags = models.ManyToManyField('Tag')
+    #Burada sınıfın adını elle yazdık. Öyle yapmasaydık sınıfları belli bir
+    # sırada yazmamız gerekirdi.
+
+    def __str__(self):
+        return self.title
